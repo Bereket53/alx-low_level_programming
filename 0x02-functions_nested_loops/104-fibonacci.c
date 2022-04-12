72165d0d1f55dd390b154a74393e2b8b0101bf36
Bereket53 <101398236+Bereket53@users.noreply.github.com>
2022-04-12 10:47:04 -0700

    fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,14 +13,14 @@ int main(void)
 	while (i < 100)
 	{
 		if (i == 0)
-			printf("%ld", j);
+			printf("%lld", j);
 		else if (i == 1)
-			printf(", %ld", k);
+			printf(", %lld", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(", %ld", k);
+			printf(", %lld", k);
 		}
 
 		++i;
