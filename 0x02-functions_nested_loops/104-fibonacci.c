4e37448893b9192227f098256ece00c283c96d60
Bereket53 <101398236+Bereket53@users.noreply.github.com>
2022-04-12 04:52:50 -0700

    task 16 fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,22 +5,20 @@
   *
   * Return: Nothing!
   */
-int main() {
+int main(void) 
+{
 
   int i;
   int n = 100;
 
-  /* initialize first and second terms*/
   int t1 = 0, t2 = 1;
 
-  /* initialize the next term (3rd term)*/
   int nextTerm = t1 + t2;
 
-  /* print the first two terms t1 and t2*/
   printf("%d, %d, ", t1, t2);
 
-  /* print 3rd to nth terms*/
-  for (i = 3; i <= n; ++i) {
+  for (i = 3; i <= n; ++i)
+  {
     printf("%d, ", nextTerm);
     t1 = t2;
     t2 = nextTerm;
